eecb82fcfd1a7b6d11e8faca5840ed9196cf811a
Shubham Darda <shubhamrdarda@gmail.com>
2024-03-09 12:02:55 +0530

    v24.3.4

---
+++ kodingwindow/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.kodingwindow.kodingwindow"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.kodingwindow.kodingwindow"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = "24.3.4"
+        setProperty("archivesBaseName", "kodingwindow")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
