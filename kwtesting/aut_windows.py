524b5773067886777769b116f28450811434377f
Shubham Darda <shubhamrdarda@gmail.com>
2024-04-03 21:21:17 +0530

    24.4.2

--- kwtesting/aut_windows.py
+++ kwtesting/aut_windows.py
@@ -5,8 +5,8 @@ kwdata = "D:/kodingwindow.github.io/_data/"
 
 start = time.time()
 try:
-    start_tests("chrome")
-    start_tests("msedge")
+    start_tests("chrome", kwdata)
+    start_tests("msedge", kwdata)
     success = True
 except:
     print("The script execution was aborted due to the following reasons: \n1. The local server isn't up and running. \n2. The required driver isn't found at the given location. \n3. Due to the mismatch of browser and driver versions \n4. If you manually intervened in the execution.")
