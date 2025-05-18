a23fe1cacd508c62e1d588c9bb98dc7d1a978c69
Shubham Darda <shubhamrdarda@gmail.com>
2025-05-18 10:14:47 +0530

    Android app

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.kodingwindow.kodingwindow"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.kodingwindow.kodingwindow"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "25.6"
+        setProperty("archivesBaseName", "kodingwindow")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.foundation.android)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
